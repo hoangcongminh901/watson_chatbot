3bdcef70944d61fe17a4ac49331ec8f2027e9816
Maho Takara <takara@jp.ibm.com>
2016-08-15 20:53:56 +0900

    update

--- vr/classify_image.js
+++ vr/classify_image.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
+//
+// Visual Recognition 単体テスト用
+//  
+//
+// 作者 Maho Takara    takara@jp.ibm.com
+//
+// Copyright (C) 2016 International Business Machines Corporation 
+// and others. All Rights Reserved. 
+// 
+// 2016/8/15  初版
+//
 
 var watson = require('watson-developer-cloud');
 var fs = require('fs');
@@ -8,9 +19,6 @@ var cred = require('./visual_recognition_credentials.json');
 var visual_recognition = watson.visual_recognition(cred.credentials);
 
 var params = {
-    //images_file: fs.createReadStream('./images/Japanese-Bob-hairstyle-for-girls.jpg')
-    //images_file: fs.createReadStream('./images/Fortified-Bicycle-Invincible-Theft-Proof-Bike-10.jpg')
-    //images_file: fs.createReadStream('./images/176.jpg')
     images_file: fs.createReadStream('./images/4731676339795.jpg')
 };
 
--- vr/detect_faces.js
+++ vr/detect_faces.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
+//
+// Visual Recognition 顔検出　単体テスト用
+//  
+//
+// 作者 Maho Takara    takara@jp.ibm.com
+//
+// Copyright (C) 2016 International Business Machines Corporation 
+// and others. All Rights Reserved. 
+// 
+// 2016/8/15  初版
+//
 
 var watson = require('watson-developer-cloud');
 var fs = require('fs');
